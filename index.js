25608bceb5da3ae33c3d1ea0cc987d19bccb903e
Binkyface <33262142+Binkyface@users.noreply.github.com>
2017-10-31 18:09:00 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const Command = require('command');
+
+module.exports = function AutoLockon(dispatch) {
+	const command = Command(dispatch);
+	let enabled = false;
+	
+	dispatch.hook('C_START_SKILL', 3, event => {
+		if (enabled && 67529864 === event.skill) {
+			event.toZ = event.z + 814.5;
+			event.toX = event.x + Math.cos(Math.PI * event.w / 32768.0) * 437;
+			event.toY = event.y + Math.sin(Math.PI * event.w / 32768.0) * 437;
+			return true;
+		}
+	});
+	
+	command.add('boomerang', () => {
+		enabled = !enabled;
+		command.message(`Boomerang is ${enabled ? 'on' : 'off'}`);
+	});
+}
